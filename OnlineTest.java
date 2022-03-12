dff7212a4980006a439db1ab9a5b02800feb91c4
Programmer20022002 <101278767+Programmer20022002@users.noreply.github.com>
2022-03-12 15:23:15 +0600

    Add files via upload

---
+++ OnlineTest.java
@@ -0,0 +1,149 @@
+package Main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+
+class OnlineTest extends JFrame implements ActionListener {
+	
+	private static final long serialVersionUID = 1L;
+
+	JLabel label;
+	JRadioButton radioButton[] = new JRadioButton[5];
+	JButton btnNext, btnBookmark;
+	ButtonGroup bg;
+	int count = 0, current = 0, x = 1, y = 1, now = 0;
+	int m[] = new int[10];
+
+	OnlineTest(String s) {
+		super(s);
+		label = new JLabel();
+		add(label);
+		bg = new ButtonGroup();
+		for (int i = 0; i < 5; i++) {
+			radioButton[i] = new JRadioButton();
+			add(radioButton[i]);
+			bg.add(radioButton[i]);
+		}
+		btnNext = new JButton("Next");
+		btnBookmark = new JButton("Bookmark");
+		btnNext.addActionListener(this);
+		btnBookmark.addActionListener(this);
+		add(btnNext);
+		add(btnBookmark);
+		set();
+		label.setBounds(30, 40, 450, 20);
+		//radioButton[0].setBounds(50, 80, 200, 20);
+		radioButton[0].setBounds(50, 80, 450, 20);
+		radioButton[1].setBounds(50, 110, 200, 20);
+		radioButton[2].setBounds(50, 140, 200, 20);
+		radioButton[3].setBounds(50, 170, 200, 20);
+		btnNext.setBounds(100, 240, 100, 30);
+		btnBookmark.setBounds(270, 240, 100, 30);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		setLocation(250, 100);
+		setVisible(true);
+		setSize(600, 350);
+	}
+
+	// handle all actions based on event
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnNext) {
+			if (check())
+				count = count + 1;
+			current++;
+			set();
+			if (current == 2) {
+				btnNext.setEnabled(false);
+				btnBookmark.setText("Result");
+			}
+		}
+		if (e.getActionCommand().equals("Bookmark")) {
+			JButton bk = new JButton("Bookmark" + x);
+			bk.setBounds(480, 20 + 30 * x, 100, 30);
+			add(bk);
+			bk.addActionListener(this);
+			m[x] = current;
+			x++;
+			current++;
+			set();
+			if (current == 2)
+				btnBookmark.setText("Result");
+			setVisible(false);
+			setVisible(true);
+		}
+		for (int i = 0, y = 1; i < x; i++, y++) {
+			if (e.getActionCommand().equals("Bookmark" + y)) {
+				if (check())
+					count = count + 1;
+				now = current;
+				current = m[y];
+				set();
+				((JButton) e.getSource()).setEnabled(false);
+				current = now;
+			}
+		}
+
+		if (e.getActionCommand().equals("Result")) {
+			if (check())
+				count = count + 1;
+			current++;
+			JOptionPane.showMessageDialog(this, "correct answers= " + count);
+			System.exit(0);
+		}
+	}
+
+	// SET Questions with options
+	void set() {
+		radioButton[4].setSelected(true);
+		if (current == 0) {
+			label.setText("1 вопрос: Что такое Джава?");
+			radioButton[0].setText("Язык программирование");
+			radioButton[1].setText("Кофе");
+			radioButton[2].setText("Книга");
+			radioButton[3].setText("Джава это айфон");
+		}
+		if (current == 1) {
+			label.setText("2 вопрос: 2+2?");
+			radioButton[0].setText("4");
+			radioButton[1].setText("5");
+			radioButton[2].setText("6");
+			radioButton[3].setText("10");
+		}
+		if (current == 2) {
+			label.setText("3 вопрос: Где ты учишься?");
+			radioButton[0].setText("Москва");
+			radioButton[1].setText("Тигр");
+			radioButton[2].setText("Ену");
+			radioButton[3].setText("Привет");
+		}
+		
+		label.setBounds(30, 40, 450, 20);
+		for (int i = 0, j = 0; i <= 90; i += 30, j++)
+			radioButton[j].setBounds(50, 80 + i, 200, 20);
+	}
+
+	// declare right answers.
+	boolean check() {
+		if (current == 0)
+			return (radioButton[0].isSelected());
+		if (current == 1)
+			return (radioButton[0].isSelected());
+		if (current == 2)
+			return (radioButton[2].isSelected());
+		
+		return false;
+	}
+
+	public static void main(String s[]) {
+		new OnlineTest("Online Test App");
+	}
+
+}
